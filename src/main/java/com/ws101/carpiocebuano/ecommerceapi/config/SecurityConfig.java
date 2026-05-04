373953f5df89d9bd3d0511cd411791a742f2f998
carpiolyndel <carpiolyndel@email.com>
2026-05-04 23:03:16 +0800

    Added Disable

--- src/main/java/com/ws101/carpiocebuano/ecommerceapi/config/SecurityConfig.java
+++ src/main/java/com/ws101/carpiocebuano/ecommerceapi/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf.disable())
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
